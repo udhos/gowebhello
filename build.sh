e53fb5ca6b1dfd0b7656f861f163ed66ea9721b4
Everton Marques <everton.marques@gmail.com>
2017-04-07 15:53:54 -0300

    Support GOPATH.

--- build.sh
+++ build.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
+[ -z "$GOPATH" ] && export GOPATH=$HOME/go
+
+echo GOPATH=$GOPATH
+
 gofmt -s -w main.go
 go tool fix main.go
 go tool vet .
-[ -x ~/go/bin/gosimple ] && ~/go/bin/gosimple main.go
-[ -x ~/go/bin/golint ] && ~/go/bin/golint main.go
-[ -x ~/go/bin/staticcheck ] && ~/go/bin/staticcheck main.go
+[ -x $GOPATH/bin/gosimple ] && $GOPATH/bin/gosimple main.go
+[ -x $GOPATH/bin/golint ] && $GOPATH/bin/golint main.go
+[ -x $GOPATH/bin/staticcheck ] && $GOPATH/bin/staticcheck main.go
 go test github.com/udhos/gowebhello
 go install github.com/udhos/gowebhello
