8723f3a5350f5ea1869b5f10021060de6981c7a7
udhos <udhos>
2017-10-24 21:25:46 -0200

    Clean-up.

--- script/staticweb.sh
+++ script/staticweb.sh
@@ -10,12 +10,6 @@ msg() {
     echo >>$log $me: $*
 }
 
-redir() {
-    while read i; do
-        msg $i
-    done
-}
-
 msg -- `date` begin
 
 msg user: `id`
@@ -24,17 +18,16 @@ msg home: $home
 
 cd $home
 
-wget -qO update-golang.sh  https://raw.githubusercontent.com/udhos/update-golang/master/update-golang.sh
+wget -qO update-golang.sh https://raw.githubusercontent.com/udhos/update-golang/master/update-golang.sh
 chmod a+rx update-golang.sh
 
 export DESTINATION=$PWD/golang
 mkdir $DESTINATION
 PROFILED=$HOME/.profile ./update-golang.sh
 
-wget -qO main.go gowebhello.go https://raw.githubusercontent.com/udhos/gowebhello/master/main.go 
+wget -qO main.go https://raw.githubusercontent.com/udhos/gowebhello/master/main.go 
 
 msg -- `date` end
 
 nohup $DESTINATION/go/bin/go run main.go >>$log 2>&1 &
-
 
